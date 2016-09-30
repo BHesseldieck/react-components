d2cb2549e2aa47dc3d26787c07fd3221804268c6
Ben Hesseldieck <b.hesseldieck@gmail.com>
2016-09-30 15:10:21 -0700

    Solved

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// TODO
+var groceries = ['Lobster', 'Cheese', 'Bananas', 'Rice', 'Beer'];
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+
+  render() {
+
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'none'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}> {this.props.item} </li>
+    );
+  }
+}
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+
+var App = (props) => (
+  <div>
+    <h1>Grocery List</h1>
+    <GroceryList items={groceries} />
+  </div>
+  );
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
